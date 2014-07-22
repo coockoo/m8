10814e514a0ecb5dc2ff96b76ea1b769901089bf
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2014-07-23 00:26:40 +0300

    Added requirejs support

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,28 @@
 /**
  * Created by coockoo on 7/22/14.
  */
+require.config({
+    shim: {
+        'jquery': {
+            exports: '$'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        }
+    },
+    paths: {
+        //'text': 'lib/text',
+        'backbone': '/bower_components/backbone/backbone',
+        'jquery': '/bower_components/jquery/dist/jquery',
+        'underscore': '/bower_components/underscore/underscore'
+        //'cookie': 'lib/jquery.cookie'
+    }
+});
 
-(function () {
-    console.log($('body'));
-})();
+require(['backbone'],function (Backbone) {
+    //Works
+});
